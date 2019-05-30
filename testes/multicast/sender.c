6965bf7c4c180ead8bd0b7cd15b76cd761036710
Daniel J. Moreira <deljuven@ymail.com>
2019-05-30 11:51:28 -0300

    first

---
+++ testes/multicast/sender.c
@@ -0,0 +1,133 @@
+//
+// Simple sender.c program for UDP
+//
+// Adapted from:
+// http://ntrg.cs.tcd.ie/undergrad/4ba2/multicast/antony/example.html
+//
+// Changes:
+// * Compiles for Windows as well as Linux
+// * Takes the port and group on the command line
+//
+// Note that what this program does should be equivalent to NETCAT:
+//
+//     echo "Hello World" | nc -u 239.255.255.250 1900
+
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h> // for sleep()
+
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MSGBUFSIZE 256
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+       printf("Command line args should be multicast group and port\n");
+       printf("(e.g. for SSDP, `sender 239.255.255.250 1900`)\n");
+       return 1;
+    }
+
+    char* group = argv[1]; // e.g. 239.255.255.250 for SSDP
+    int port = atoi(argv[2]); // 0 if error, which is an invalid port
+
+    // !!! If test requires, make these configurable via args
+    //
+    const int delay_secs = 1;
+    const char *message = "Hello, World! From sender.";
+
+    // create what looks like an ordinary UDP socket
+    //
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    // allow multiple sockets to use the same PORT number
+    //
+    u_int yes = 1;
+    if ( setsockopt( fd, SOL_SOCKET, SO_REUSEADDR, (char*) &yes, sizeof(yes) ) < 0)
+    {
+       perror("Reusing ADDR failed");
+       return 1;
+    }
+
+    // set up destination address
+    //
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(group);
+    addr.sin_port = htons(port);
+
+    // bind to receive address
+    //
+    if (bind(fd, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+        perror("bind");
+        return 1;
+    }
+
+    // use setsockopt() to request that the kernel join a multicast group
+    //
+    struct ip_mreq mreq;
+    mreq.imr_multiaddr.s_addr = inet_addr(group);
+    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+    if (
+        setsockopt(
+            fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char*) &mreq, sizeof(mreq)
+        ) < 0
+    ){
+        perror("setsockopt");
+        return 1;
+    }
+
+      // now just sendto() our destination!
+    int nbytes = 0;
+    while (1) {
+        char ch = 0;
+        nbytes = sendto(
+            fd,
+            message,
+            strlen(message),
+            0,
+            (struct sockaddr*) &addr,
+            sizeof(addr)
+        );
+        if (nbytes < 0) {
+            perror("sendto");
+            return 1;
+        }
+        printf("\t\tmensagem enviada de sender\n");
+        sleep(delay_secs); // Unix sleep is seconds
+
+
+        char msgbuf[MSGBUFSIZE];
+        int addrlen = sizeof(addr);
+        nbytes = recvfrom(
+            fd,
+            msgbuf,
+            MSGBUFSIZE,
+            0,
+            (struct sockaddr *) &addr,
+            &addrlen
+        );
+        if (nbytes < 0) {
+            perror("recvfrom");
+            return 1;
+        }
+        msgbuf[nbytes] = '\0';
+        printf("\t\tmensagem recebida de sender\n");
+        puts(msgbuf);
+        sleep(delay_secs); // Unix sleep is seconds
+
+     }
+
+    return 0;
+}
